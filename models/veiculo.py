9ee61e9c6762fcac925d48ef46dfbd655805ae2d
hevertongomes <evertinho10.hg@gmail.com>
2025-10-06 19:45:28 -0300

    feat:implementando validate para placa com sqlmodel

--- models/veiculo.py
+++ models/veiculo.py
@@ -2,6 +2,8 @@ from typing import Optional
 
 from sqlmodel import Field, SQLModel
 
+from pydantic import validator
+
 
 class Veiculo(SQLModel, table=True):
     __tablename__: str = 'veiculos'
@@ -15,3 +17,9 @@ class Veiculo(SQLModel, table=True):
     descricao: str
     preco_fip: float
     preco_loja: float
+
+    @validator('placa')
+    def validar_placa(cls, value: str):
+        if len(value) < 8:
+            raise ValueError('A placa deve ter 8 digítos.')
+        return value
